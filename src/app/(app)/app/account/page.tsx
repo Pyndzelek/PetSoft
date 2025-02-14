0bd50ae22ef226509fa6515f452c034d3d806cd0
kamil <kamilmaslanka212@gmail.com>
2025-02-14 02:20:21 +0100

    created account UI

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,5 +1,16 @@
+import ContentBlock from "@/components/content-block";
+import H1 from "@/components/h1";
 import React from "react";
 
 export default function AccountPage() {
-  return <div>AccountPage</div>;
+  return (
+    <main>
+      <H1>Your Account</H1>
+
+      <ContentBlock className="h-[500px] flex flex-col justify-center items-center">
+        <p>Logged in as:</p>
+        <p>Adress email</p>
+      </ContentBlock>
+    </main>
+  );
 }
