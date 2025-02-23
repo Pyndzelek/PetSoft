f84ef13fa325b759a014c4ab73df8a64be3a9012
kamil <kamilmaslanka212@gmail.com>
2025-02-23 19:56:50 +0100

    Middleware setup

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -5,16 +5,22 @@ import Link from "next/link";
 export default function SignUpPage() {
   return (
     <main>
-      <H1 className="text-center text-black mb-4 -mt-3">Sign up</H1>
+      <H1 className="mb-4 -mt-3 text-black text-center">Sign up</H1>
 
       <AuthForm />
 
-      <p className="mt-6 text-sm text-zinc-500 text-center">
-        Already have an account?{" "}
-        <Link href="/login" className="text-bold text-zinc-700">
-          Log in
-        </Link>
-      </p>
+      <SignInPrompt />
     </main>
+  );
+}
+
+function SignInPrompt() {
+  return (
+    <p className="mt-6 text-sm text-zinc-500 text-center">
+      Already have an account?{" "}
+      <Link href="/login" className="text-zinc-700 text-bold">
+        Log in
+      </Link>
+    </p>
   );
 }
