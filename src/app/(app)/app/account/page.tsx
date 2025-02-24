9419497dc145eeaabfb9aa3a332e45cd3114ad35
kamil <kamilmaslanka212@gmail.com>
2025-02-24 01:00:45 +0100

    Updated the user email adress on account page

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,15 +1,22 @@
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
 import React from "react";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function AccountPage() {
+export default async function AccountPage() {
+  const session = await auth();
+  if (!session?.user) {
+    redirect("/login");
+  }
+
   return (
     <main>
       <H1 className="mt-[72px] mb-[8px]">Your Account</H1>
 
       <ContentBlock className="h-[500px] flex flex-col justify-center items-center">
         <p>Logged in as:</p>
-        <p>Adress email</p>
+        <p>{session.user.email}</p>
       </ContentBlock>
     </main>
   );
